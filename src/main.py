4099ee5b56335ef28caf1154d7c233602708e8b5
Emanuel Maia <tavanomanu@gmail.com>
2023-02-09 14:05:40 +0000

    Basic functionality for backspace feature. Need to fix a few bugs

--- src/main.py
+++ src/main.py
@@ -4,6 +4,9 @@ from colorama import Fore, Back, Style
 from os import system, name
 from time import time
 
+ESCAPE = chr(27)
+BACKSPACE = chr(127)
+
 def getch():
     fd = sys.stdin.fileno()
     old_settings = termios.tcgetattr(fd)
@@ -20,45 +23,71 @@ def clearScreen() -> None:
 def pickText() -> str:
     with open("../docs/texts.txt") as f:
         lines = f.readlines()
-        text = lines[randint(0, len(lines) - 1)]
+        # text = lines[randint(0, len(lines) - 1)]
+        # test line
+        text = lines[17]
         if text.endswith('\n'):
             text = text[:len(text) - 1]
         return text
 
-def displayText(text: str, current: int, status: bool) -> None:
+def displayText(text: str, current: int, wrong_start: int, wrong_span: int, status: bool) -> None:
     if status:
         print(text[:current] + Back.WHITE + Fore.BLACK + text[current] + 
         Style.RESET_ALL + Style.DIM + text[current+1:] + Style.RESET_ALL)
     else:
-        print(text[:current] + Back.RED + text[current] + 
-        Style.RESET_ALL + Style.DIM + text[1+current:] + Style.RESET_ALL)
+        print(text[:wrong_start] + Back.RED + text[wrong_start:wrong_start+wrong_span] + 
+        Style.RESET_ALL + Style.DIM + text[wrong_start+wrong_span:] + Style.RESET_ALL)
 
+"""
+TODO:  
+    - FIX ISSUE WHERE WHEN THE LAST FEW CHARACTERS ARE WRONG, IT
+    DOESN'T ALLOW YOU TO MISTYPE FURTHER THAN THE PENULTIMATE 
+    CHARACTER (IT SHOULD ALLOW YOU TO TYPE UNTIL THE LAST ONE)
+
+    - FIX THE ISSUE WHERE WHEN YOU BACKSPACE AND DELETE THE MISTYPED
+    CHARACTER, THERE'S NO CURSOR HIGHLIGHTING (SHOULD BE WHITE
+    HIGHLIGHTING SINCE THERE ARE NO MORE WRONG CHARACTERS)
+"""
 def run() -> None:
     text = pickText()
-    wordCount = len(text.split())
-
+    word_count = len(text.split())
     tic = 0.0
     errors = 0
+    curr_wrong = 0
     correct = True
+    wrong_idx = -1
+
     idx = 0
-    while idx < len(text):
+    while idx < len(text) or curr_wrong > 0:
         clearScreen()
+        print("Press 'esc' to quit")
         print(f"Time: {round(time() - tic if tic > 0 else tic, 1)}\n")
-        displayText(text, idx, correct)
+        displayText(text, idx, wrong_idx, curr_wrong, correct)
         
-        character = getch()
+        char = getch()
+        if char == ESCAPE:
+            quit()
+
         if idx == 0 and correct:
             tic = time()
 
-        if character == text[idx]:
+        if char == text[idx] and curr_wrong == 0:
             correct = True
             idx += 1
-        else:
+        elif not correct and curr_wrong > 0 and char == BACKSPACE:
+            curr_wrong -= 1
+            idx -= 1
+        elif char != BACKSPACE and curr_wrong < 7:
+            if curr_wrong == 0:
+                wrong_idx = idx
+            if idx < len(text) - 1:
+                idx += 1
+                curr_wrong += 1
             errors += 1
             correct = False
     toc = time()
 
-    print(f"WPM: {round(wordCount / ((toc - tic) / 60), 0)}")
+    print(f"WPM: {round(word_count / ((toc - tic) / 60), 0)}")
     print(f"Accuracy: {round((1 - errors / len(text)) * 100, 1)}%")
 
 if __name__ == "__main__":
